208242988ed991b7c1980acc83e815e84b78d1b7
snehsanjgaik <116013665+snehsanjgaik@users.noreply.github.com>
2022-10-17 16:27:08 +0100

    Update Python_bikeshare.py

--- Python_bikeshare.py
+++ Python_bikeshare.py
@@ -1,3 +1,11 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
 
 def get_filters():
     """
@@ -177,4 +185,6 @@ def main():
         if restart.lower() != 'yes':
             break
 
+if __name__ == "__main__":
+	main()
 
